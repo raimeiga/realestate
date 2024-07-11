62b4876e5060a279de88a73e5855abf3b5024542
raimeiga <yusuke612@hotmail.com>
2024-07-12 05:32:00 +0900

    いろいろ調整中

--- js/main.js
+++ js/main.js
@@ -19,11 +19,24 @@ $('.header-nav-btn').hover(
 
 
 
-  // カルーセル
-  $('.mv-slick').slick({
+  // スライダー
+  $('.mv-image-wrap').slick({
     autoplay: true,  //自動再生
     dots: true,      //ドットインジケーター
     infinite: true,
-    autoplaySpeed: 50,  //自動再生の切り替え速度
+    autoplaySpeed: 5000,  //自動再生の切り替え速度
+    slidesToShow: 3,
     arrows: false,  //左右矢印
   });
+
+
+  $(function () {
+    $(".mv-image-wrap").slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true,
+        dots: true,
+        slidesToShow: 3,
+        speed: 2000,
+    });
+});
