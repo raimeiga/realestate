ba0d44d577193094f2da6b4315d24a03eeedd97e
raimeiga <yusuke612@hotmail.com>
2024-07-12 22:32:32 +0900

    いろいろ調整中

--- js/main.js
+++ js/main.js
@@ -1,44 +1,36 @@
 
+// // ヘッダーの動き
+// $(function(){
+// $('.header-nav-btn').hover(
+//   function() {
+//       $(this).animate({
+//             opacity: 0.5,}),
+//       $(this).css(
+//        'border-bottom', 'solid 1px #fff'
+//       )
+//     },
+//     function() {
+//       $(this).animate({
+//             opacity: 1}),
+//       $(this).css(
+//         'border-bottom', 'none'
+//       )
+//     }
+//   );
+// });
 
-$('.header-nav-btn').hover(
-  function() {
-      $(this).animate({
-            opacity: 0.5,}),
-      $(this).css(
-       'border-bottom', 'solid 1px #fff'
-      )
-    },
-    function() {
-      $(this).animate({
-            opacity: 1}),
-      $(this).css(
-        'border-bottom', 'none'
-      )
-    }
-  );
 
+// メインビジュアルのスライド
 
 $(function(){
   // スライダー
-  $('.mv-image-wrap').slick({
+  $('.mv-image').slick({
     autoplay: true,  //自動再生
     dots: true,      //ドットインジケーター
     infinite: true,
-    autoplaySpeed: 5000,  //自動再生の切り替え速度
+    autoplaySpeed: 3000,  //自動再生の切り替え速度
     slidesToShow: 3,
     arrows: true,  //左右矢印
   });
 
 });
-
-
-//   $(function () {
-//     $(".mv-image-wrap").slick({
-//         autoplay: true,
-//         autoplaySpeed: 3000,
-//         arrows: true,
-//         dots: true,
-//         slidesToShow: 3,
-//         speed: 2000,
-//     });
-// });
